1de803d206a2f925d121200fa4195b7f38f1aec0
wade <wade@angleorange.com>
2022-03-09 21:46:53 +0200

    search-bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,11 +14,13 @@ import {MatTabsModule } from '@angular/material/tabs';
 import {MatIconModule } from '@angular/material/icon'
 import {MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
+import { SearchBarComponent } from './search-bar/search-bar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchBarComponent
   ],
   imports: [
     BrowserModule,
